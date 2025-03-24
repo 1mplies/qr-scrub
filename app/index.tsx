3278b9df87a580a7851cb25c4ee21deb73a240a8
wissamhadajarb <wissam.hadjarab@gmail.com>
2025-03-24 21:10:24 +0100

    trying to pull

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,3 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
 import { useState } from "react";
 import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 import { useRouter } from "expo-router";
